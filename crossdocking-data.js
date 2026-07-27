563b018e6eafd434786e2d7fbdef5a54b1289be9
ecommerce-guimepa <bruno@guimepa.com.br>
2026-07-27 17:54:00 -0300

    Add files via upload

---
+++ crossdocking-data.js
@@ -0,0 +1,407 @@
+const crossDockingData = [
+  {
+    "ordem": 1,
+    "fornecedor": "BRITANIA",
+    "image": "britania.png",
+    "prazo": 7,
+    "operador": "GERAL",
+    "preco": null,
+    "custo": null
+  },
+  {
+    "ordem": 2,
+    "fornecedor": "BEL",
+    "image": "Bel.png",
+    "prazo": 4,
+    "operador": "GERAL",
+    "preco": null,
+    "custo": null
+  },
+  {
+    "ordem": 3,
+    "fornecedor": "TEKNA",
+    "image": "Tekna.png",
+    "prazo": 3,
+    "operador": "GERAL",
+    "preco": null,
+    "custo": null
+  },
+  {
+    "ordem": 4,
+    "fornecedor": "BETA",
+    "image": "Beta.png",
+    "prazo": 7,
+    "operador": "GERAL",
+    "preco": null,
+    "custo": null
+  },
+  {
+    "ordem": 5,
+    "fornecedor": "O.V.D",
+    "image": "Vonder.png",
+    "prazo": 3,
+    "operador": "ABAIXO",
+    "preco": 20,
+    "custo": 12
+  },
+  {
+    "ordem": 6,
+    "fornecedor": "O.V.D",
+    "image": "Vonder.png",
+    "prazo": 2,
+    "operador": "ACIMA",
+    "preco": 20,
+    "custo": 12
+  },
+  {
+    "ordem": 7,
+    "fornecedor": "O.V.D",
+    "image": "Vonder.png",
+    "prazo": 1,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 8,
+    "fornecedor": "WAP",
+    "image": "Wap.png",
+    "prazo": 10,
+    "operador": "ABAIXO",
+    "preco": 100,
+    "custo": 60.5
+  },
+  {
+    "ordem": 9,
+    "fornecedor": "WAP",
+    "image": "Wap.png",
+    "prazo": 7,
+    "operador": "ACIMA",
+    "preco": 100,
+    "custo": 60.5
+  },
+  {
+    "ordem": 10,
+    "fornecedor": "WAP",
+    "image": "Wap.png",
+    "prazo": 5,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 11,
+    "fornecedor": "MAKITA",
+    "image": "Makita.png",
+    "prazo": 12,
+    "operador": "ABAIXO",
+    "preco": 200,
+    "custo": 121
+  },
+  {
+    "ordem": 12,
+    "fornecedor": "MAKITA",
+    "image": "Makita.png",
+    "prazo": 7,
+    "operador": "ACIMA",
+    "preco": 200,
+    "custo": 121
+  },
+  {
+    "ordem": 13,
+    "fornecedor": "MAKITA",
+    "image": "Makita.png",
+    "prazo": 4,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 14,
+    "fornecedor": "MAKITA",
+    "image": "Makita.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 2000,
+    "custo": 1212
+  },
+  {
+    "ordem": 15,
+    "fornecedor": "NEGRAO",
+    "image": "WORKER.png",
+    "prazo": 12,
+    "operador": "ABAIXO",
+    "preco": 50,
+    "custo": 30
+  },
+  {
+    "ordem": 16,
+    "fornecedor": "NEGRAO",
+    "image": "WORKER.png",
+    "prazo": 8,
+    "operador": "ACIMA",
+    "preco": 50,
+    "custo": 30
+  },
+  {
+    "ordem": 17,
+    "fornecedor": "NEGRAO",
+    "image": "WORKER.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 500,
+    "custo": 303
+  },
+  {
+    "ordem": 18,
+    "fornecedor": "TOYAMA",
+    "image": "Toyama.png",
+    "prazo": 5,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 19,
+    "fornecedor": "TOYAMA",
+    "image": "Toyama.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 20,
+    "fornecedor": "TOYAMA",
+    "image": "Toyama.png",
+    "prazo": 2,
+    "operador": "ACIMA",
+    "preco": 2000,
+    "custo": 1212
+  },
+  {
+    "ordem": 21,
+    "fornecedor": "MTX",
+    "image": "MTX.png",
+    "prazo": 4,
+    "operador": "ABAIXO",
+    "preco": 20,
+    "custo": 12
+  },
+  {
+    "ordem": 22,
+    "fornecedor": "MTX",
+    "image": "MTX.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 20,
+    "custo": 12
+  },
+  {
+    "ordem": 23,
+    "fornecedor": "CSM",
+    "image": "CSM.png",
+    "prazo": 7,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 24,
+    "fornecedor": "CSM",
+    "image": "CSM.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 25,
+    "fornecedor": "TRAMONTINA",
+    "image": "Tramontina.png",
+    "prazo": 4,
+    "operador": "ABAIXO",
+    "preco": 200,
+    "custo": 121
+  },
+  {
+    "ordem": 26,
+    "fornecedor": "TRAMONTINA",
+    "image": "Tramontina.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 200,
+    "custo": 121
+  },
+  {
+    "ordem": 27,
+    "fornecedor": "CCM",
+    "image": "CCM.png",
+    "prazo": 5,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 28,
+    "fornecedor": "CCM",
+    "image": "CCM.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 29,
+    "fornecedor": "GMEG",
+    "image": "GMEG.png",
+    "prazo": 5,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 30,
+    "fornecedor": "GMEG",
+    "image": "GMEG.png",
+    "prazo": 3,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 31,
+    "fornecedor": "BUFFALO",
+    "image": "Buffalo.png",
+    "prazo": 5,
+    "operador": "ABAIXO",
+    "preco": 500,
+    "custo": 303
+  },
+  {
+    "ordem": 32,
+    "fornecedor": "BUFFALO",
+    "image": "Buffalo.png",
+    "prazo": 4,
+    "operador": "ACIMA",
+    "preco": 500,
+    "custo": 303
+  },
+  {
+    "ordem": 33,
+    "fornecedor": "BUFFALO",
+    "image": "Buffalo.png",
+    "prazo": 2,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 34,
+    "fornecedor": "PRESTO",
+    "image": "PRESTO.png",
+    "prazo": 10,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 35,
+    "fornecedor": "PRESTO",
+    "image": "PRESTO.png",
+    "prazo": 7,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 36,
+    "fornecedor": "BRANCO",
+    "image": "Branco.png",
+    "prazo": 6,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 37,
+    "fornecedor": "BRANCO",
+    "image": "Branco.png",
+    "prazo": 4,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 38,
+    "fornecedor": "BEL",
+    "image": "Bel.png",
+    "prazo": 15,
+    "operador": "produtos produção",
+    "preco": null,
+    "custo": null
+  },
+  {
+    "ordem": 39,
+    "fornecedor": "BOVENAU",
+    "image": "BOVENAU.png",
+    "prazo": 8,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 40,
+    "fornecedor": "BOVENAU",
+    "image": "BOVENAU.png",
+    "prazo": 4,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 41,
+    "fornecedor": "BALMER",
+    "image": "Balmer.png",
+    "prazo": 12,
+    "operador": "ABAIXO",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 42,
+    "fornecedor": "BALMER",
+    "image": "Balmer.png",
+    "prazo": 7,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  },
+  {
+    "ordem": 43,
+    "fornecedor": "BOSCH",
+    "image": "Bosch.png",
+    "prazo": 8,
+    "operador": "ABAIXO",
+    "preco": 100,
+    "custo": 60.5
+  },
+  {
+    "ordem": 44,
+    "fornecedor": "BOSCH",
+    "image": "Bosch.png",
+    "prazo": 7,
+    "operador": "ACIMA",
+    "preco": 100,
+    "custo": 60.5
+  },
+  {
+    "ordem": 45,
+    "fornecedor": "BOSCH",
+    "image": "Bosch.png",
+    "prazo": 6,
+    "operador": "ACIMA",
+    "preco": 1000,
+    "custo": 606
+  }
+];
