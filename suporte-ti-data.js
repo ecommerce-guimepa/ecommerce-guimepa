d2edbbdc910149c7b2c911d133ef05adda7326c2
ecommerce-guimepa <bruno@guimepa.com.br>
2026-07-30 15:32:01 -0300

    Add files via upload

---
+++ suporte-ti-data.js
@@ -0,0 +1,380 @@
+const suporteTiData = [
+  {
+    "melhoria": "API",
+    "modulo": "API FORNECEDORES",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET PUBLICACAO DE PRODUTOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "LUCAS",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API INTELIPOST",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API BI",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET ENTRADA PEDIDOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET TELA DE TRANSPORTES",
+    "responsavelTi": "LÉO",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET ENVIO DE PREÇOS E ESTOQUE",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET MERCADO ENVIOS E TRANSMISSÕES",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET CAPTA DADOS DOS PRODUTOS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API DADOS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "LUCAS",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET SAÍDA DE PEDIDOS",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "API",
+    "modulo": "API ANYMARKET FULL",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "COMPRAS",
+    "modulo": "COMPRAS TELEVENDAS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "FERNANDO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "COMPRAS",
+    "modulo": "COMPRAS ECOMMERCE",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "COMPRAS",
+    "modulo": "COMPRAS COLETAS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "EDUARDO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "DADOS",
+    "modulo": "TABELAS FORNECEDORES",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "DADOS",
+    "modulo": "COLUNAS, FILTROS E CALL-BACK",
+    "responsavelTi": "LÉO",
+    "backupTi": "RESPONSÁVEL DO SETOR",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "DADOS",
+    "modulo": "CADASTROS GERAIS - PRODUTOS - FORNECEDORES",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL RELATORIOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL CADASTRO E CONFIGURACAO DE TRIBUTOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL RADAR",
+    "responsavelTi": "MATEUS",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "CONHECIMENTOS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL PEDIDOS",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL NOTA FISCAL SAÍDA",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "FISCAL SEFAZ",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "FISCAL",
+    "modulo": "DEVOLUÇÕES CLIENTE E FORNECEDOR",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "LUANA",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "OPERACAO",
+    "modulo": "FINANCEIRO",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "GISLAINE",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "OPERACAO",
+    "modulo": "RMA",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "THAYS",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "SISTEMA",
+    "modulo": "LIBERACAO DE ACESSOS DOS USUARIOS A DADOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "RESPONSÁVEL DO SETOR",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "NILSA",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "SISTEMA",
+    "modulo": "SERVIDOR LENTIDÃO",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "SISTEMA",
+    "modulo": "CRON",
+    "responsavelTi": "RESPONSÁVEL DO SETOR",
+    "backupTi": "LÉO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "VENDAS",
+    "modulo": "CRM PEDIDOS MARGEM E IPV",
+    "responsavelTi": "LÉO",
+    "backupTi": "MATEUS",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "VENDAS",
+    "modulo": "CRM PEDIDOS",
+    "responsavelTi": "LÉO",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "BRUNO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "VENDAS",
+    "modulo": "CRM AUDITORIA FRETES",
+    "responsavelTi": "LÉO",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "IGOR",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "VENDAS",
+    "modulo": "ANALISES ORÇAMENTO, PEDIDOS, FINANCEIRO",
+    "responsavelTi": "MATEUS",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "NILSA",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "VENDAS",
+    "modulo": "CRM NOTAS FISCAIS",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "WMS",
+    "modulo": "NOTAS RECEBIDAS",
+    "responsavelTi": "MATEUS",
+    "backupTi": "TARCÍSIO",
+    "responsavelSetor": "EDUARDO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "WMS",
+    "modulo": "SEPARAÇÃO DE PEDIDOS",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "EDUARDO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "WMS",
+    "modulo": "ESTOQUE, BALANÇO, MOVIMENTACOES",
+    "responsavelTi": "TARCÍSIO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "EDUARDO",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "OUTROS",
+    "modulo": "EXCEL - TEAMS - EMAILS - TRELLO - SENHAS",
+    "responsavelTi": "LÉO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "BRUNO",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "OUTROS",
+    "modulo": "HARDWARE - COMPUTADORES - IMPRESSORAS - REDE",
+    "responsavelTi": "LÉO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "LUCAS",
+    "observacoes": ""
+  },
+  {
+    "melhoria": "OUTROS",
+    "modulo": "PROJETOS POWER BI",
+    "responsavelTi": "LÉO",
+    "backupTi": "LÉO",
+    "responsavelSetor": "JEAN",
+    "backupSetor": "JEAN",
+    "observacoes": ""
+  }
+];
