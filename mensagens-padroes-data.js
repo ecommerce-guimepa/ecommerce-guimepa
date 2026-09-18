5a95b03c023dd59bf0d32ac419c06e6a3359f9b4
ecommerce-guimepa <bruno@guimepa.com.br>
2026-09-18 12:08:00 -0300

    Add files via upload

---
+++ mensagens-padroes-data.js
@@ -0,0 +1,128 @@
+﻿const mensagensPadroesData = [
+  {
+    "tema": "Despacho | Envio",
+    "opcoes": [
+      {
+        "assunto": "DESPACHO",
+        "mensagem": "Bom dia!\n\nNossos pedidos são enviados em até 5 dias úteis. Mas não se preocupe!\nEste prazo já está calculado no prazo de entrega informado em sua compra.\nAssim que despachado, você receberá a NF e os dados para rastreamento.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "DESPACHO",
+        "mensagem": "Bom dia!\n\nTivemos um problema no lote das mercadorias que tínhamos em estoque e tivemos que solicitar novas peças ao fornecedor. \nEstamos aguardando chegar e assim que isso acontecer, iremos priorizar o seu pedido.\nA previsão é que receba seu item em até 10 dias úteis.\nInfelizmente, se não puder aguardar o prazo, solicitamos que abra a reclamação junto ao Mercado Livre como arrependimento, para que possamos autorizar o estorno dos seus valores e seguir com o cancelamento do pedido.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Devolução | Troca",
+    "opcoes": [
+      {
+        "assunto": "DEVOLUÇÃO | TROCA | SOLICITANDO FOTOS",
+        "mensagem": "Prezado (a) cliente,\n\nAgradecemos por entrar em contato conosco e lamentamos qualquer problema que tenha tido com a remessa.\n\nAntes de darmos o aceite na sua solicitação, precisamos confirmar o estado do produto.\nPara que possamos proceder com a devolução, cancelamento ou troca do produto, é necessário que o mesmo esteja sem uso e na embalagem original do fabricante,  juntamente com todos os acessórios e manuais.\nPor gentileza, nos envie fotos e vídeos para análise do produto, com todos os acessórios, manuais e embalagem original do fabricante.\n*Motivo da devolução:\n*Fotos do produto recebido.\n*Fotos das etiquetas da embalagem.\n*Fotos das avarias (caso o produto apresente avarias).\n*Fotos do produto embalado.\n*Descrição detalhada do defeito com o produto (caso apresente).\nAssim que confirmarmos o estado do produto, daremos continuidade na tratativa do caso.\nEstamos à disposição para qualquer dúvida ou esclarecimento adicional que você possa ter.\n\nSegue nosso WhatsApp para o envio das imagens  (41) 99887-0991.\n\nPor gentileza, se identifique com o número do pedido ou Nota Fiscal.\n\nEm caso de dúvidas, ficaremos à disposição!\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "DEVOLUÇÃO | TROCA | SOLICITANDO FOTOS",
+        "mensagem": "Prezada cliente, bom dia!\n\nAgradecemos por entrar em contato conosco e lamentamos qualquer problema que tenha tido com a remessa.\n\nPor gentileza, nos envie fotos e vídeos  do produto, da embalagem e das etiquetas.\n\nAssim que enviado, daremos continuidade na tratativa do caso.\nEstamos à disposição para qualquer dúvida ou esclarecimento adicional que você possa ter.\n\nSegue nosso WhatsApp para o envio das imagens  (41) 99887-0991.\nPor gentileza, se identifique com o número do pedido ou Nota Fiscal.\n\nEm caso de dúvidas, ficaremos à disposição!\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Estoque | Cancelamento",
+    "opcoes": [
+      {
+        "assunto": "FALTA DE ESTOQUE",
+        "mensagem": "Bom dia, Prezado @Cliente,\n\nSentimos muito informar que o produto adquirido está atualmente fora de estoque. Dessa forma, solicitamos gentilmente que abra uma solicitação de cancelamento para que possamos proceder com o reembolso.\n\nPermanecemos à disposição para quaisquer dúvidas e aguardamos o seu retorno.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "RESPOSTA PARA PEDIDO CROSS DOCKING | ATRASO DE DESPACHO",
+        "mensagem": "Prezado (a) cliente, boa tarde!\n\nO seu pedido está em processo de preparação para envio.\n\nUtilizamos do prazo de 5 a 10 dias úteis para despachar os produtos.\n\nFique tranquilo (a) , pois esse prazo está incluso na data estimada para entrega, que é até o dia / /2024.\nAssim que a mercadoria for despachada, o cliente receberá um E-mail com a nota Fiscal e os dados de rastreio.\n\nEm caso de qualquer dúvida ou problema, estamos à disposição para ajudá-lo no que for\nnecessário e pode nos chamar no Whatsapp GUIMEPA (41) 99887-0991 que teremos o maior prazer em lhe atender.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Fornecedor | Atraso",
+    "opcoes": [
+      {
+        "assunto": "ATRASO FORNECEDOR",
+        "mensagem": "Bom dia Prezado Cliente ,\n\nPedimos desculpas pelo atraso, devido a alta demanda de vendas o fabricante acabou atrasando os envios aos lojistas, ocasionando em atrasos no despacho.\n\nEstamos cobrando o mesmo para que possamos despachar sua encomenda no início da próxima semana e ou até o final desta semana.\n\nFicamos a disposição.\n\nAtenciosamente,\nEquipe | GUIMEPA."
+      },
+      {
+        "assunto": "ATRASO FORNECEDOR",
+        "mensagem": "Boa tarde,\n\nDevido a um atraso por parte do fabricante, houve um impacto no prazo de envio das encomendas para nossos clientes.\n\nPedimos desculpas pelo ocorrido e informamos que a previsão de despacho da sua encomenda é entre os dias @@/@@/2026.\n\nGostaríamos de saber se você consegue aguardar mais alguns dias?\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Reclamação | Estorno",
+    "opcoes": [
+      {
+        "assunto": "ABRIR RECLAMAÇÃO ML PARA ESTORNO",
+        "mensagem": "Boa tarde!\nPara autorizarmos seu estorno, precisamos que abra uma reclamação junto ao mercado Livre.\nPara isso,  pode acessar: Minhas compras e, na compra, em \"Ajuda com a compra\", ele deverá escolher as opções que melhor descrevam o que aconteceu em seu caso > ao final, poderá escrever detalhadamente o que ocorreu e anexar uma mensagem, se preferir. https://www.mercadolivre.com.br/ajuda"
+      },
+      {
+        "assunto": "ABRIR RECLAMAÇÃO ML PARA ESTORNO",
+        "mensagem": "Bom dia! Para autorizarmos o estorno, é necessário abrir uma reclamação junto ao Mercado Livre.\n\nAcesse Minhas compras > selecione a compra > Ajuda com a compra e escolha as opções que melhor descrevem o ocorrido. Ao final, você poderá detalhar o caso e anexar uma mensagem, se necessário.\n\nhttps://www.mercadolivre.com.br/ajuda"
+      }
+    ]
+  },
+  {
+    "tema": "Transportadora | Avaria",
+    "opcoes": [
+      {
+        "assunto": "TRANSPORTADORA INDENIZAR AVARIA SEM RESSALVA",
+        "mensagem": "Considerando o disposto no Código de Defesa do Consumidor (Lei 8.078/90), especialmente em seus arts. 12, 14 e 26, informamos que o cliente possui o direito de inspecionar o produto entregue e comunicar eventuais avarias ou vícios dentro do prazo legal de até 7 dias corridos, conforme entendimento consolidado na jurisprudência.\n\nMesmo que não tenha havido ressalva expressa no ato da entrega, eventuais danos ocultos ou não constatados de imediato devem ser aceitos como passíveis de reclamação, desde que informados dentro desse prazo. A exigência de ressalva imediata como única forma de indenização é prática abusiva, pois limita um direito legal do consumidor, além de desconsiderar situações comuns de entrega a terceiros ou restrição operacional dos entregadores quanto à conferência da mercadoria no local.\n\nDessa forma, a transportadora se compromete a assumir a responsabilidade pelos danos decorrentes de avarias no transporte, desde que:\n\nO destinatário ou responsável pela compra tenha informado o problema à empresa contratante no prazo de até 7 dias corridos após o recebimento;\n\nHaja comprovação documental ou fotográfica da avaria, que será compartilhada com a transportadora para fins de análise e reembolso ou substituição do item.\n\nReforçamos que a responsabilidade pela integridade da mercadoria durante o transporte é objetiva e solidária, conforme previsto nos artigos mencionados, não podendo ser transferida exclusivamente ao consumidor ou à loja contratante."
+      },
+      {
+        "assunto": "TRANSPORTADORA INDENIZAR AVARIA SEM RESSALVA",
+        "mensagem": "Considerando o disposto no Código de Defesa do Consumidor (Lei nº 8.078/90), especialmente em seus arts. 12, 14 e 26, o consumidor possui o direito de verificar o produto recebido e comunicar eventuais avarias ou vícios dentro do prazo legal aplicável.\n\nA ausência de ressalva no momento da entrega não impede, por si só, a reclamação posterior, especialmente quando se tratar de dano oculto ou não constatado imediatamente. A exigência de conferência e ressalva imediata como única condição para responsabilização não pode afastar os direitos assegurados ao consumidor.\n\nDessa forma, a transportadora deverá assumir a responsabilidade por avarias comprovadamente decorrentes do transporte, desde que:\n\nO problema seja comunicado à empresa contratante em até 7 dias corridos após o recebimento;\nSejam apresentados registros fotográficos ou demais documentos que comprovem a avaria, para análise e eventual reembolso ou substituição.\n\nRessaltamos que a responsabilidade pela integridade da mercadoria durante o transporte não pode ser integralmente transferida ao consumidor ou à loja contratante, devendo ser observadas as responsabilidades previstas na legislação consumerista."
+      }
+    ]
+  },
+  {
+    "tema": "Postagem | Correios",
+    "opcoes": [
+      {
+        "assunto": "POSTAGEM VIA CORREIOS",
+        "mensagem": "Prezado (a) cliente,\n\nCódigo Autorização: *NUMERO COD\n\nValido até: ##/##/####\n\nPara devolução do produto, basta levá-lo, devidamente embalado, em uma agência dos Correios e fazer a postagem com a autorização acima.\nVocê não terá nenhum custo referente ao frete, a autorização de postagem é válida pelo período de 10 dias,  sendo até o dia  ##/##/####.\n\nApós essa data, não será mais permitida a postagem.\n\nORIENTAÇÕES PARA A DEVOLUÇÃO:\n\n1. Acondicione o(s) produto(s) na embalagem original, dentro de uma caixa de papelão ou envoltório que o(s) proteja, acobertando qualquer\netiqueta que tenha na embalagem.\n2. Devolva junto com o(s) produto(s) os manuais, certificados e acessórios;\n3. Leve o(s) produto(s) até a agência dos Correios autorizada e informe o número do código ao atendente.\n\nEsta autorização só é aceita em agências próprias dos Correios, identificadas pela sigla AC. Veja a lista completa no site  http://www2.correios.com.br/sistemas/agencias/.\n\nGuarde o comprovante de postagem até que a sua solicitação tenha sido totalmente atendida.\n\nFicaremos no aguardo da postagem do produto e em caso de dúvidas, estamos à disposição!\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "POSTAGEM VIA CORREIOS",
+        "mensagem": "Prezado(a) cliente,\n\nCódigo de autorização: NUMERO COD\nVálido até: ##/##/####\n\nPara devolver o produto, basta levá-lo devidamente embalado a uma agência dos Correios e informar o código acima. Não haverá custo de frete. A autorização é válida por 10 dias, até ##/##/####. Após essa data, a postagem não será aceita.\n\nORIENTAÇÕES PARA DEVOLUÇÃO:\n\nEmbale o produto na embalagem original, dentro de caixa de papelão ou envoltório adequado, cobrindo eventuais etiquetas.\nEnvie junto todos os manuais, certificados e acessórios.\nNa agência, informe o código de autorização ao atendente.\n\n⚠️ A autorização é válida somente em agências próprias dos Correios (AC).\nConsulte a lista de agências no site oficial dos Correios.\n\nGuarde o comprovante de postagem até a conclusão da solicitação.\n\nAguardamos a postagem do produto. Em caso de dúvidas, estamos à disposição!\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Reclame Aqui",
+    "opcoes": [
+      {
+        "assunto": "RECLAME AQUI",
+        "mensagem": "Bom dia @‌Cliente\n\nPrimeiramente, lamentamos o ocorrido em sua compra! Atuamos sempre de maneira transparente na solução junto ao cliente na intenção da satisfação do mesmo. Infelizmente existem problemas gerais, os quais resolvemos 100% dos casos.\n\nReferente ao seu caso, foi repassado toda orientação conforme política de atendimento, infelizmente por algum motivo resolveu abrir essa reclamação, mas fique tranquilo(a) que seu problema será resolvido considerando os fatos informados e dentro do Código de Proteção e Defesa do Consumidor.\n\nTodas as informações sobre o caso serão informadas pelo canal de sua compra ou pelo meio de comunicação que entrou em contato conosco.\n\nContinuamos trabalhando para que você e outros clientes tenham uma boa experiência em sua próxima compra conosco.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "RECLAME AQUI",
+        "mensagem": "Olá Sr. @Cliente,\n\nPrimeiramente, gostaríamos de nos desculpar pelos transtornos ocasionados.\n\nInfelizmente por conta das chuvas e deslizamentos houve atrasos nas coletas dos produtos em nossa expedição, porém assim que liberado o pedido prosseguiu em viagem, e está em rota de entrega conforme informado via WhatsApp.\n\nO grupo Guimepa trabalha de uma forma honesta e transparente com seus clientes visando sempre dar todo atendimento e suporte necessário para os devidos problemas.\n\nQualquer dúvida, estamos à disposição para mais esclarecimentos.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Reembolso Indevido",
+    "opcoes": [
+      {
+        "assunto": "REEMBOLSO INDEVIDO | CANCELAMENTO MKTP",
+        "mensagem": "Bom dia,\n\nGostaríamos de informá-lo(a) que identificamos um reembolso indevido relacionado ao seu pedido em nosso canal de compra. Para evitar qualquer procedimento adicional por parte do nosso setor jurídico financeiro, solicitamos sua cooperação para ajustar o pagamento.\n\nAgradecemos antecipadamente pela sua atenção e aguardamos seu retorno breve para resolvermos essa pendência.\n\nAtenciosamente,\nEquipe | GUIMEPA"
+      },
+      {
+        "assunto": "REEMBOLSO INDEVIDO | CANCELAMENTO MKTP",
+        "mensagem": "Bom dia,\n\nIdentificamos um reembolso indevido relacionado ao seu pedido realizado em nosso canal de vendas.\n\nPara regularizarmos a pendência e evitarmos a necessidade de adoção de outras medidas pelo nosso setor jurídico/financeiro, solicitamos sua colaboração para realizar o pagamento do valor correspondente.\n\nAgradecemos pela atenção e aguardamos seu retorno para que possamos solucionar a situação da melhor forma possível.\n\nAtenciosamente,\nEquipe GUIMEPA"
+      }
+    ]
+  },
+  {
+    "tema": "Full | Devolução",
+    "opcoes": [
+      {
+        "assunto": "RETORNO FULL - IDENTIFICAÇÃO",
+        "mensagem": "Para conseguir identificar sobre qual venda se trata essa devolução, ou você pode analisar pela etiqueta amarela colada no produto, nela tem o número da venda. Ou por esse caminho aqui:\n\nminha conta > anúncios > gestão de estoque Full > controle de estoque > baixar relatório de estoque > relatório de devoluções ao centro de distribuição.\n\nO caminho para abrir a reclamação é esse aqui: Vendas > Filtra o número da venda > 3 pontinhos > Retirei o produto, mas ele não está nas condições esperadas."
+      }
+    ]
+  }
+];
